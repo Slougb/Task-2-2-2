96f015d3e8eef5c3e453a1745f7289b025a32b28
Maxim Subbotin <subbotinmax01@gmail.com>
2025-01-17 22:19:12 +0800

    добавил аннотацию @Service связал с помощью DI с controller

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +14,15 @@ import java.util.List;
 @Controller
 public class CarController {
 
+    private CarServiceImpl carService;
 
+    @Autowired
+    public CarController(CarServiceImpl carService) {
+        this.carService = carService;
+    }
 
     @GetMapping("/cars")
     public String showCar(@RequestParam(value = "count", required = false, defaultValue = "5") int a , Model model) {
-        CarServiceImpl carService = new CarServiceImpl();
         List<Car> cars = carService.getCars(a);
         model.addAttribute("cars", cars);
         return "cars";
